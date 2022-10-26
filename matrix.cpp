3e118113dc56b2d7dde10fd2ceff17c1cc3956f3
Prince-Rauniyar1 <105070241+Prince-Rauniyar1@users.noreply.github.com>
2022-10-26 16:54:19 +0545

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //getting input of rows and columns from the user
+    int m,n;
+    cout<<"Enter the number of rows: ";
+    cin>>m;
+    cout<<endl;
+    cout<<"Enter the number of columns: ";
+    cin>>n;
+    int matrix [m][n];
+    //rows and columns done!
+
+    for(int i=1;i<=m;++i){            //input for first matrix
+        for(int j=1; j<=n;++j){
+            cout <<"a"<<i<<j<<": ";
+            cin>>matrix[i][j];
+        }
+    }
+
+    cout<<"Now for the second matrix\n";
+    
+    int matrix2[m][n];                //input for second matrix
+    for(int i=1;i<=m;++i){
+        for(int j=1; j<=n;++j){
+            cout<<"a"<<i<<j<<": ";
+            cin>>matrix2[i][j];
+        }
+    }
+
+    cout<<"Sum goes here\n";
+    int sum[m][n];                    //processing sum
+    for(int i=1;i<=m;++i){            
+        for(int j=1; j<=n;++j){
+            sum[i][j]=matrix[i][j]+matrix2[i][j];
+        }
+    }
+
+    for(int i=1;i<=m;++i){            //outputting sum to the user
+        for(int j=1; j<=n;++j){
+         cout<< sum[i][j]<<" ";
+         if(j==n){
+            cout<<endl;
+         }
+    }
+
+    }
+    cout<<"Program ending well!\n";
+    return 0;   
+}
