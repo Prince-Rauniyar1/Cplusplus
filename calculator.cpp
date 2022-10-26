2134c2081e0b38e15e721e31a6f1560a73d20244
Prince-Rauniyar1 <105070241+Prince-Rauniyar1@users.noreply.github.com>
2022-10-26 16:52:32 +0545

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float n1,n2;
+    char op;
+    float result;
+    cout<<"Enter the first number: ";
+    cin>>n1;
+    cout<<endl;
+     cout<<"Enter the second number: ";
+    cin>>n2;
+    cout<<endl;
+    cout<<"Enter the operator: ";
+    cin>>op;
+    
+    cout<<endl;
+    switch(op){
+        case '+':
+        result = n1+n2;
+        cout<<result<<endl;
+        break;
+
+        case '-':
+        cout<<n1-n2<<endl;
+        break;
+        
+        case '*':
+        cout<<n1*n2<<endl;
+        break;
+
+        case '/':
+        result = n1/n2;
+        cout<<result<<endl;
+        break;
+
+        default: 
+        cout<<"Invalid Operator!"<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
